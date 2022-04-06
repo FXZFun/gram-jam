1824400fc3a28794bac29ac85740e38795255f90
Cole Chamberlin <cchamberlin@apptek.com>
2022-04-06 08:06:30 -0700

    add leaderboard take 2

---
+++ src/Leaderboard.svelte
@@ -0,0 +1,87 @@
+<script lang='ts'>
+  import Modal from './Modal.svelte';
+  import Word from './Word.svelte';
+  import type { GameRecord, Match } from './types';
+  import ActionButton from './ActionButton.svelte';
+  import Streak from './Streak.svelte';
+  import WordChain from './WordChain.svelte';
+  
+  // export let lost: boolean;
+  // export let onReset: () => void;
+  // export let bestStreak: number;
+  // export let bestChain: number;
+  // export let bestWords: Match[];
+  // export let score: number;
+  export let open: boolean;
+  export let onClose: () => void;
+  let shareText: string;
+  let copied = false;
+  let games: GameRecord[];
+  
+  $: {
+    if (open) {
+      games = JSON.parse(localStorage.getItem('games')) ?? [];
+      games = games
+        .filter(g => g.score)
+        .sort((a, b) => b.score - a.score)
+        .slice(0, 10);
+    }
+  }
+  
+</script>  
+
+<Modal open={open} onClose={onClose}>
+  <div class=container>
+    <h1>Your Leaderboard</h1>
+    <table>
+    {#each games.sort((a, b) => b.score - a.score) as game}
+      <tr>
+        <td><h3>
+          {new Date(game.date).toLocaleDateString('en-US', { 
+            month: 'short',
+            day: 'numeric'
+          })}
+        </h3></td>
+        <td><h3>{game.score}</h3></td>
+      </tr>
+    {/each}
+    </table>
+    <div class=controls>
+      <ActionButton onClick={onClose}>Close</ActionButton>
+    </div>
+  </div>
+</Modal>
+
+<style>
+  table {
+    width: 100%;
+    text-align: start;
+    margin-bottom: 8px;
+  }
+  table h3 {
+    margin: 0.5em;
+  }
+  .score {
+    font-weight: bold;
+  }
+  .container {
+    padding: 2em;
+  }
+  .controls {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+  }
+  .result {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .spacer {
+    width: 1em;
+  }
+  .grow {
+    flex-grow: 1;
+  }
+</style>
